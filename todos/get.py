15d10f2c979ecd61f5fc0476e0fcb280fa9d5a64
EC2 Default User <ec2-user@ip-172-31-36-234.ec2.internal>
2021-01-21 22:21:00 +0000

    ggggggggggggg

--- todos/get.py
+++ todos/get.py
@@ -5,7 +5,7 @@ from todos import decimalencoder
 import boto3
 dynamodb = boto3.resource('dynamodb')
 
-
+##### uuuuuuuuuuuuuuuu
 def get(event, context):
     table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
 
