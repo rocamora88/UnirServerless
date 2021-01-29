92bdd3dd832ddc505826bd828f89bc0345a4138c
Jose Rocamora <rocamora88@hotmail.com>
2021-01-29 20:50:55 +0000

    ya existe

--- todos/get.py
+++ todos/get.py
@@ -5,7 +5,7 @@ from todos import decimalencoder
 import boto3
 dynamodb = boto3.resource('dynamodb')
 
-### ssssssssss
+
 ## primera carga en develop
 def get(event, context):
     table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
